5b062e0578db35b01fec4494f445999f51758553
debasish283 <debasishmisra3008@gmail.com>
2019-04-16 20:26:40 +0530

    changed port

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import router from "./routes/routes";
 import mongoose from "mongoose";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1234;
 require('dotenv').config();
 
 
